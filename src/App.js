9c4c429f255c298c45633f96762f8e98ce7b344a
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-07-31 15:21:09 -0700

    Change in structure

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React, { Component } from 'react';
 import { icons } from './config';
 import './App.css';
 
-import Icon from './node_modules/components/Icon';
-import TextField from './components/TextField';
-
-
+import Icon from './Icon';
 
 class App extends Component {
   state = {
     size: "small",
     color: "#666"
   }
-
-  _handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
-
+  
   render() {
     const iconsElements = icons.map( (icon, index) => {
       return <div key={index} className="icon-box">
@@ -33,24 +26,6 @@ class App extends Component {
       <div className="App">
         <div className="icon-container">
           { iconsElements }
-        </div>
-        <div className="side-panel">
-          <TextField 
-            type="text"
-            label="change size"
-            name="size"
-            placeholder="small is the default"
-            value={this.state.size} 
-            onChange={this._handleChange}
-          />
-
-          <TextField 
-            type="text"
-            label="change color"
-            name="color"
-            value={this.state.color} 
-            onChange={this._handleChange}
-          />
         </div>
       </div>
     );

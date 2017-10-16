8343db2374a523dfaf8b4d46f08528fce30f4293
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-10-16 13:46:15 -0700

    Update react 16, other dependencies  and necessary changes

--- .storybook/addons.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-import '@storybook/addon-links/register';
-import '@storybook/addon-knobs/register';
-import 'storybook-readme/register';

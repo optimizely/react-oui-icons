39169fec9b55fb82ed1d156194097a46ef6dfb6c
David Mann <david.mann@optimizely.com>
2019-02-20 11:53:27 -0600

    Increase Storybook icon and icon title size

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ const Container = styled.div`
 const styles = {
   icon: {
     alignSelf: "center",
+    width: 32,
+    height: 32,
   }
 }
 
@@ -40,7 +42,7 @@ stories.add('All icons', withInfo()(() => {
           transition: all 0.4s ease;
           content: '${icon.title}';
           font-family: Helvetica;
-          font-size: 10px;
+          font-size: 16px;
           position: absolute;
           bottom: 30px;
         }
